05bf424d851ceaddfcda6ddb9b0d75ea4e80a207
Diego Quintero <daquinterop@gmail.com>
2024-06-04 15:05:49 +0000

    modified setup.py to include packages and modlues

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="dssatservice"
+    name="dssatservice",
+    package_dir={
+        "dssatservice": "/home/dquintero/dssat_service/dssat_service/"
+    },
+    packages=['dssatservice', 'dssatservice.data', 'dssatservice.ui'],
+    py_modules=["dssatservice.database", "dssatservice.dssat"]
 )
